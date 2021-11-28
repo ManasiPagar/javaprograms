4f459d852e3c6f238edf3a6618f4452d3bbc88c8
Manasi Pagar <95023646+ManasiPagar@users.noreply.github.com>
2021-11-28 20:15:27 +0530

    Add files via upload

---
+++ SwappNumber.java
@@ -0,0 +1,25 @@
+/**
+ * @(#)SwappNumber.java
+ *
+ *
+ * @author 
+ * @version 1.00 2021/11/28
+ */
+
+
+public class SwappNumber
+ {
+
+    public static void main(String[]args)
+    {
+    	int a=10;
+    	int b=20;
+    	a=a+b;
+    	b=a-b;
+    	a=a-b;
+    	System.out.println("After Swapping A is"+a+" B is "+b);
+    	
+    }
+    }
+    
+    
